d48b79825c369bc54eb62aa2f9870dda70b4ff7b
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 <frankdafrica@gmail.com>
2025-05-29 17:42:47 -0700

    t

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,18 @@
 
 const request = require('request');
 
-// Get the movie ID from command line arguments
 const movieId = process.argv[2];
 
-// Check if movie ID is provided
 if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  console.error('Please provide a movie ID');
   process.exit(1);
 }
 
-// Star Wars API base URL
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Function to make HTTP request and return a promise
-function makeRequest(url) {
+function makeRequest (url) {
   return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`HTTP ${response.statusCode}: ${response.statusMessage}`));
+    request(url, (err, res, body) => {
+      if (err || res.statusCode !== 200) {
+        reject(err || new Error(`Status: ${res.statusCode}`));
       } else {
         resolve(JSON.parse(body));
       }
@@ -29,38 +21,18 @@ function makeRequest(url) {
   });
 }
 
-// Function to fetch character name from character URL
-async function getCharacterName(characterUrl) {
+async function main () {
   try {
-    const character = await makeRequest(characterUrl);
-    return character.name;
-  } catch (error) {
-    console.error(`Error fetching character: ${error.message}`);
-    return null;
-  }
-}
-
-// Main function to fetch and display characters
-async function displayCharacters() {
-  try {
-    // Fetch movie data
-    const movie = await makeRequest(apiUrl);
+    const film = await makeRequest(`https://swapi-api.alx-tools.com/api/films/${movieId}/`);
     
-    // Get character URLs from the movie data
-    const characterUrls = movie.characters;
-    
-    // Fetch all character names in order
-    for (const characterUrl of characterUrls) {
-      const characterName = await getCharacterName(characterUrl);
-      if (characterName) {
-        console.log(characterName);
-      }
+    for (const url of film.characters) {
+      const character = await makeRequest(url);
+      console.log(character.name);
     }
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error('Error:', error.message);
     process.exit(1);
   }
 }
 
-// Execute the main function
-displayCharacters();
+main();
